e4f0ba5e6da236cdaeed6e161869eea9d5b776c2
TavinhoSilva06 <tavinho.augusto.amaral@gmail.com>
2024-10-09 09:35:36 -0300

    Criando uma API

---
+++ src/app.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  @Get("outroendpoint2")
+  getOutroEndPoint(): string {
+    return "Meu outro end point";
+  }
+}
