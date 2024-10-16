72fe4de11207e2bf200eae13a08aef35708255f0
TavinhoSilva06 <tavinho.augusto.amaral@gmail.com>
2024-10-16 09:13:19 -0300

    Todos os metodos dos verbos API rest

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Patch, Post, Delete } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Post("outroendpoint")
+  postOutroEndpoint(): string {
+    return "Novo end point";
+  }
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Patch("endpointatualizado")
+  patchOutroendPoint(): string {
+    return "End point atualizado";
+  }
+
+ @Delete("exluindoendpoint")
+ deleteOutroEndpoint(): string {
+  return "Apagando End Point"
+ }
+
   @Get("outroendpoint2")
   getOutroEndPoint(): string {
     return "Meu outro end point";
   }
+  
 }
+
+
