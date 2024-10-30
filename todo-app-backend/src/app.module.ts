4329b11e7f8979c84b58a0e4f9f06740557bceba
TavinhoSilva06 <tavinho.augusto.amaral@gmail.com>
2024-10-29 21:17:28 -0300

    Criando uma API do zero

---
+++ todo-app-backend/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TodoModule } from './app/todo/todo.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configservice: ConfigService) => ({
+        type: 'mariadb',
+        host: configservice.get('DB_HOST', 'localhost'),
+        port: Number(configservice.get('DB_PORT', 3306)),
+        username: configservice.get('DB_USERNAME', 'root'),
+        password: configservice.get('DB_PASSWORD', '123'),
+        database: configservice.get('DB_DATABASE', 'todo'),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+    TodoModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ todo-app-backend/src/app/todo/todo.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity({ name: "todos" })
+export class TodoEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  task: string;
+
+  @Column({ name: "is_done", type: "tinyint", width: 1 })
+  isDone: number;
+
+  @CreateDateColumn({ name: "created_at" })
+  createAdt: string;
+
+  @UpdateDateColumn({ name: "uptaded_at" })
+  updateAdt: string;
+
+  @DeleteDateColumn({ name: "deleted_at" })
+  deleteAdt: string;
+}
